916fa73e38e8327cc60af63cf058469b59f22903
Lucas Goyeche <lucas@raxel.co>
2023-02-10 23:46:04 +0900

    stop on background.js if there is no sender.tab

--- src/js/background.js
+++ src/js/background.js
@@ -1,5 +1,6 @@
 window.perfWatch = {};
 
 chrome.runtime.onMessage.addListener(function (message, sender, sendResponse) {
+  if (!sender.tab) return;
   window.perfWatch[sender.tab.id] = message.essential || null;
 });
